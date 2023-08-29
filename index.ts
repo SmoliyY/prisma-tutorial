623424eb2a3da3d1dacc333f14bbb705a9a7bb35
SmoliyY <yevhen.smoliy@gmail.com>
2023-08-29 11:13:16 +0300

    remote changes

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-
+console.log('che che chenges')
 const main = async () => {
   await prisma.post.deleteMany();
   await prisma.user.deleteMany();
