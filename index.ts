eb40cdee4ccb255bea7b0128d695e1799831be9b
Yevhen Smoliy <52038205+SmoliyY@users.noreply.github.com>
2023-09-25 17:50:49 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-console.log('hey6')
+console.log('hey10')
 
 const main = async () => {
   await prisma.post.deleteMany();
